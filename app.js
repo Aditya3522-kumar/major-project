e82f8b44c95aaece52c65f4afdc2eaae0db2e491
AK <akgupta3522@gmail.com>
2024-12-08 23:22:02 +0530

    update .env

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-// const MONGO_URL = process.env.MONGO_URL;	
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URL;	
 
 
 main().then(()=>{
--- init/index.js
+++ init/index.js
@@ -3,28 +3,78 @@ const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 const Review = require("../models/reviews.js");
 const dotenv = require("dotenv");
+
+// Load environment variables from .env file
 dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL;
 console.log(MONGO_URL);
 
-main().then(()=>{
-    console.log("database connected");
-})
-.catch((err)=>{
-    console.log("not connected" , err);
-})
-async function main(){
-    await mongoose.connect(MONGO_URL);
+// Sample reviews data
+const sampleReviews = [
+  {
+    comment: "Amazing place, beautiful views!",
+    rating: 5,
+  },
+  {
+    comment: "Good location but a bit expensive",
+    rating: 4,
+  },
+  {
+    comment: "Decent stay, could be better maintained",
+    rating: 3,
+  },
+  {
+    comment: "Wonderful experience, highly recommended!",
+    rating: 5,
+  },
+  {
+    comment: "Nice spot for a weekend getaway",
+    rating: 4,
+  }
+];
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-const initDB = async()=>{
-    await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    await Review.deleteMany({});
+const initDB = async () => {
+  // Clear existing data
+  await Listing.deleteMany({});
+  await Review.deleteMany({});
 
+  // Insert listings
+  const listings = await Listing.insertMany(initData.data);
+  
+  // Add some reviews to each listing
+  for (let listing of listings) {
+    // Randomly select 2-4 reviews for each listing
+    const numReviews = Math.floor(Math.random() * 3) + 2;
+    const reviews = [];
+    
+    for (let i = 0; i < numReviews; i++) {
+      const randomReview = sampleReviews[Math.floor(Math.random() * sampleReviews.length)];
+      const review = new Review(randomReview);
+      await review.save();
+      reviews.push(review);
+    }
+    
+    // Update listing with reviews
+    listing.reviews = reviews;
+    await listing.save();
+  }
 
-    console.log("data initialised");
-
+  console.log("data was initialized with reviews");
 };
-initDB();
+
+initDB().then(() => {
+  mongoose.connection.close();
+});
